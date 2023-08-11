52380044b4147999f356de31fbbe8fee76d846b8
Crowdbotics <engineering@crowdbotics.com>
2023-08-11 07:29:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-butterfly-de-108430",
+  url: "https://shiny-butterfly-de-108430.botics.co",
+  api: "https://shiny-butterfly-de-108430.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
